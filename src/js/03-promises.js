ef7972f26c3dfc4b3d4aa8f6a7120a276e163a65
KatarzynaBat <kaisa.wrona@gmail.com>
2023-12-12 16:10:23 +0100

    onepromise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+const form = document.querySelector('.form');
+let firstDelay = form.delay;
+let stepDelay = form.step;
+let amount = form.amount;
+const submit = document.querySelector('.submit');
+
+// deklaracja obietnicy
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    firstDelay = form.delay.value;
+    stepDelay = form.step.value;
+    amount = form.amount.value;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve('resolved');
+      } else {
+        reject('rejectes');
+      }
+    }, firstDelay);
+  });
 }
+// wywiałanie obietnicy
+
+submit.addEventListener('click', event => {
+  event.preventDefault();
+
+  console.log(stepDelay);
+  createPromise()
+    .then(value => {
+      console.log(value);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
