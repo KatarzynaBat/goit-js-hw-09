dd510a5aab75fad45e6d132c10beb7efbbf41582
KatarzynaBat <kaisa.wrona@gmail.com>
2023-12-13 09:49:05 +0100

    poprawki zad 1 i 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -65,11 +65,13 @@ function convertMs(ms) {
     // Remaining days
     const days = addLeadingZero(Math.floor(ms / day));
     // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
     // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
     // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    const seconds = addLeadingZero(
+      Math.floor((((ms % day) % hour) % minute) / second)
+    );
 
     return { days, hours, minutes, seconds };
   }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,11 +2,11 @@ const form = document.querySelector('.form');
 let delay;
 let stepDelay;
 let amount;
-const submit = document.querySelector('.submit');
+const submitBtn = document.querySelector('.submit');
 
 // losowanie i zwracanie wyniku
 
-function getChances(id, delay) {
+function createPromise(id, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (Math.random() > 0.3) {
@@ -25,15 +25,14 @@ function handleClick(event) {
   stepDelay = Number(form.step.value);
   delay = Number(form.delay.value);
   for (let i = 1; i <= amount; i++) {
-    getChances(i, delay + stepDelay * (i - 1))
+    createPromise(i, delay + stepDelay * (i - 1))
       .then(({ id, delay }) => {
         console.log(`Fulfilled promise ${id} in ${delay}ms`);
       })
-
       .catch(({ id, delay }) => {
         console.log(`Rejected promise ${id} in ${delay}ms`);
       });
   }
 }
 
-submit.addEventListener('click', handleClick);
+form.addEventListener('submit', handleClick);
